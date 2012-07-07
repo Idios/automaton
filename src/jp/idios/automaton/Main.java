b8957bbd835f2fce4d5d24a8e6ed68dfa251b494
Idios <idiosam@gmail.com>
2012-07-07 23:53:57 +0900

    Final state definition

--- src/jp/idios/automaton/Main.java
+++ src/jp/idios/automaton/Main.java
@@ -1,5 +1,8 @@
 package jp.idios.automaton;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 	private State current = State.INITIAL;
@@ -8,12 +11,36 @@ public class Main {
 		current = current.accept();
 	}
 
+	private void start(final String string) {
+		ExecutorService service = Executors.newFixedThreadPool(1);
+		current = current.run();
+		Thread runnable = new Thread() {
+			@Override
+			public void run() {
+				// do something
+			}
+		};
+		service.execute(runnable);
+		try {
+			runnable.join();
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		} finally {
+			service.shutdown();
+			current = current.accept();
+		}
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Main machine = new Main();
-		machine.start();
+		if (args.length > 0) {
+			machine.start(args[0]);
+		} else {
+			machine.start();
+		}
 		System.out.println("State machine is " + machine.current);
 	}
 
